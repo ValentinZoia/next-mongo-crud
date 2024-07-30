64a841eee53f5e07866a6f950093611d5e51dffc
ValentinZoia <valentinzoia@gmail.com>
2024-07-30 18:45:39 -0300

    add api url

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-// const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1/next-tasks";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1/next-tasks";
 
-const MONGO_URI =  'mongodb+srv://valentinzoia:valentinzoiapassword@cluster0.jbd3gin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 
 export const connectDB = async () => {
     try {
